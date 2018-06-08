ecc41dd437f876decd7e41f9ed3f75e4016e75c4
MacStudent <macstudent@20X-iMacXX-M-395.local>
2018-06-08 17:04:17 -0400

    Car class added"

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -4,6 +4,7 @@ public class Car extends Vehicle implements IPrintable {
     public String printMyData()
     {
         return "Car =" + getMake() + ", Plate=" + getPlate() + "]";
+        //jihbjhbkj
     }
 
 }
