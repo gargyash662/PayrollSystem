f6fcd6ffde56d6aba5b9151ae1d44280dc5e9789
yash <gargyash662@gmail.com>
2018-06-13 19:32:49 -0400

    Main class created for displaying data

--- src/main/java/com/payroll/CommissionBasedPartTime.java
+++ src/main/java/com/payroll/CommissionBasedPartTime.java
@@ -1,4 +1,33 @@
 package com.payroll;
 
-public class CommissionBasedPartTime {
+import com.payroll.PartTime;
+
+public class CommissionBasedPartTime extends PartTime implements IPrintable {
+
+    private double commissionPerc;
+
+    public double getCommissionPerc()
+    {
+        return commissionPerc;
+    }
+
+    public void setCommissionPerc(double commissionPerc)
+    {
+        this.commissionPerc = commissionPerc;
+    }
+
+    @Override
+    public double calcEarning()
+    {
+        return (getRate() * getHoursWorked()) + ((commissionPerc / 100) * (getRate() * getHoursWorked()));
+    }
+
+    @Override
+    public String printMyData()
+    {
+        return " Commission=" + commissionPerc + ", Rate=" + getRate() + ", HoursWorked="
+                + getHoursWorked() + ", Name=" + getName() + ", Age=" + getAge() + ", vehicle="
+                + getVehicle().printMyData() + "]";
+    }
+
 }
--- src/main/java/com/payroll/FullTime.java
+++ src/main/java/com/payroll/FullTime.java
@@ -1,4 +1,44 @@
 package com.payroll;
 
-public class FullTime {
+import com.payroll.Employee ;
+
+public class FullTime extends Employee implements IPrintable {
+
+    private double salary;
+    private double bonus;
+
+    public double getSalary()
+    {
+        return salary;
+    }
+
+    public void setSalary(double salary)
+    {
+        this.salary = salary;
+    }
+
+    public double getBonus()
+    {
+        return bonus;
+    }
+
+    public void setBonus(double bonus)
+    {
+        this.bonus = bonus;
+    }
+
+    @Override
+    public double calcEarning()
+    {
+        return salary + bonus;
+    }
+
+    @Override
+    public String printMyData()
+    {
+        return "Salary= " + salary + ", Bonus=" + bonus + ", Name=" + getName() + ", Age=" + getAge()
+                + ", vehicle=" + getVehicle().printMyData() +"]";
+    }
+
+
 }
